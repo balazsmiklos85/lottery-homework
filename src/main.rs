1b53bdc8122d4435b4dfb0b1e3d9a5cf141bb07f
Miklós Balázs <balazsmiklos85@gmail.com>
2018-07-06 17:15:23 +0200

    C boilerplate added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    //#include <stdio.h>
+    //int main() {
+    //    int a,b,c,d,e;
+    //    printf("READY\n");
+    //    int i = 5;
+    //    while(i == 5) {
+    //        i = scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    //        if(i == 5) {
+    //            printf("%d %d %d %d\n", 0, 1, 2, 3);
+    //        }
+    //    }
+    //    return 0;
+    //}
 }
