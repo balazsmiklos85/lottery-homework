841220b07a6876f4d661a59433e868c27306cc74
Miklós Balázs <balazsmiklos85@gmail.com>
2018-07-06 18:26:29 +0200

    input reading partially added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Result};
+use std::collections::HashSet;
+
+enum ErrorCodes {
+    WRONG_PARAMETERS,
+    IO_ERROR,
+}
 
 fn main() {
     let file_name = get_input_file_name();
+    let file_reader = FileReader { name: file_name };
+    let input = file_reader.read();
     println!("READY");
     //#include <stdio.h>
     //int main() {
@@ -27,7 +37,73 @@ fn get_input_file_name() -> String {
         None => {
             eprintln!("Wrong amount of arguments");
             eprintln!("Usage: ./lottery_homework input_file.name");
-            std::process::exit(1);
+            std::process::exit(exit_code(ErrorCodes::WRONG_PARAMETERS));
         },
+    }
+}
+
+fn exit_code(error_code: ErrorCodes) -> i32 {
+    match error_code {
+        ErrorCodes::WRONG_PARAMETERS => return 1,
+        ErrorCodes::IO_ERROR => return 2,
+    }
+}
+
+struct FileReader {
+    name: String,
+}
+
+impl FileReader {
+    fn read(self) -> LotteryGames {
+        let mut result = LotteryGames::new();
+        let input_file = match File::open(self.name.to_string()) {
+            Ok(f) => f,
+            Err(e) => {
+               eprintln!("Cannot open input file: {}", self.name); 
+               eprintln!("{}", e);
+               std::process::exit(exit_code(ErrorCodes::IO_ERROR)); 
+            },
+        };
+        for line in BufReader::new(input_file).lines() {
+            match line {
+                Ok(l) => result.add(LotteryGame::from_line(l)),
+                Err(e) => {
+                    eprintln!("Error while reading file: {}", self.name); 
+                    eprintln!("{}", e);
+                    std::process::exit(exit_code(ErrorCodes::IO_ERROR));                     
+                }
+            }            
+        }
+        return result;
+    }
+}
+
+struct LotteryGames {
+    games: Vec<LotteryGame>
+}
+
+impl LotteryGames {
+    fn new() -> LotteryGames {
+        return LotteryGames { games: Vec::new() };
+    }
+
+    fn add(&mut self, game: LotteryGame) {
+        self.games.push(game);
+    }
+}
+
+struct LotteryGame {
+    numbers: HashSet<i32>,
+}
+
+impl LotteryGame {
+    fn new() -> LotteryGame {
+        return LotteryGame { numbers: HashSet::new() };
+    }
+
+    fn from_line(line: String) -> LotteryGame {
+        let mut result = LotteryGame::new();
+        //TODO: handle line
+        return result;
     }
 }
